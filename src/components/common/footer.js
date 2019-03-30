4b778d8e482b2bfac6d639fda97eb13bfba79ff4
Hristo Raydovski <h.raydovski@infoleven.com>
2019-03-30 17:59:13 +0200

    fix footer

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => (
-    <footer className="footer mt-3 p-3 fixed-bottom">
+    <footer className="footer mt-3 p-3">
         <div className="container-fluid">
             <div className="text-muted container-fluid text-center">&copy; Business ideas developing system.
                 All rights reserved.
