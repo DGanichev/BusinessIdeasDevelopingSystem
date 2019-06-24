94cc6baf4d6087e5b6d969388726fe3e4a4c1c1a
Dimitar Ganichev <dganichev97@gmail.com>
2019-06-24 16:17:46 +0300

    Updates

---
+++ precache-manifest.f2e10f862a58bb7292d2a89dec58779e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f88f0efc4f8a1879fe72",
+    "url": "/BusinessIdeasDevelopingSystem/static/js/runtime~main.9957e15a.js"
+  },
+  {
+    "revision": "8f71531de65fc5a2f331",
+    "url": "/BusinessIdeasDevelopingSystem/static/js/main.371a505f.chunk.js"
+  },
+  {
+    "revision": "3cfaa31db3fbba3f8226",
+    "url": "/BusinessIdeasDevelopingSystem/static/js/2.bc4cc710.chunk.js"
+  },
+  {
+    "revision": "8f71531de65fc5a2f331",
+    "url": "/BusinessIdeasDevelopingSystem/static/css/main.3ba36ff4.chunk.css"
+  },
+  {
+    "revision": "3cfaa31db3fbba3f8226",
+    "url": "/BusinessIdeasDevelopingSystem/static/css/2.d911d090.chunk.css"
+  },
+  {
+    "revision": "f9b0c2c7d95d83e82be12013a5bd9741",
+    "url": "/BusinessIdeasDevelopingSystem/index.html"
+  }
+];
